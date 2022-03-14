06bfcce0666fa92eab41b37c46d34f2a15bd10e1
BleM3e <nguyenbenjamin.bn@gmail.com>
2022-03-14 09:03:43 +0100

    Adaptation du code récupérer pour les multiensembles

--- Advanced_Programming/Tp_list_chainee_recursif/function.c
+++ Advanced_Programming/Tp_list_chainee_recursif/function.c
@@ -1,3 +1,29 @@
 #include "struct.h"
 #include <stdio.h>
 
+void ajout_tete (Multi_ens val, Liste *pL){
+    Liste p_tmp = malloc(sizeof(Liste));
+    p_tmp->valeur = val;
+    p_tmp->suivant = *pL;
+    *pL = p_tmp;
+}
+
+void affiche_liste (Liste l){
+    while (NULL != l){
+        printf("P: %p\tV: %d\tM: %d\n",l,l->valeur.entier,l->valeur.multiplicité);
+        l = l->suivant;
+    }
+}
+
+void supprime_tete (Liste *pL){
+    Liste p_tmp = (*pL)->suivant;
+    free(*pL);
+    *pL = p_tmp;
+}
+
+void desalloue_liste (Liste *pL){
+    while (NULL != *pL){
+        supprime_tete(pL);
+    }
+}
+
--- Advanced_Programming/Tp_list_chainee_recursif/struct.h
+++ Advanced_Programming/Tp_list_chainee_recursif/struct.h
@@ -1,7 +1,19 @@
 #ifndef __STRUCT_H__
 #define __STRUCT_H__
 
+typedef struct multi_ensemble
+{
+    int entier;
+    int multiplicité;
+} Multi_ens;
 
+typedef struct cellule_t
+{
+    Multi_ens valeur;
+    struct cellule_t * suivant;
+} Cellule;
+
+typedef Cellule *Liste;
 
 
 #endif
